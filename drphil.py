d5358788d8eaa95dccf5000de55c3ae3d0990545
Joris Jamers <jjamers@its.jnj.com>
2023-01-15 17:21:03 +0100

    init commit

---
+++ drphil.py
@@ -0,0 +1,35 @@
+import discord
+import os
+
+d = {}
+TOKEN =  os.environ.get('DISCORD_TOKEN')
+ 
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):    
+    print("message-->", message)
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+
+    msg = message.content.lower().split(" ")
+    for i in msg: 
+        
+        with open("quotes.txt") as f:
+            for line in f:
+                (key, val) = line.split(":")
+                d[(key)] = val
+                
+    for key in d:
+        if key in msg: 
+            await message.channel.send((d[key]))    
+ 
+client.run(TOKEN)
